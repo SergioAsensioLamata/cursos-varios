f4d8626f6c2eebae4c2c24c9bdf82dce8063133a
i-am-sergio <sergioasensiolamata@hotmail.com>
2024-11-16 18:27:48 +0100

    added conditions in the budget

--- react-native/planificador/App.js
+++ react-native/planificador/App.js
@@ -1,13 +1,27 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Alert } from 'react-native';
 import Header from './src/components/Header';
 import NuevoPresupuesto from './src/components/NuevoPresupuesto';
 
 export default function App() {
+
+  const handleNuevoPresupuesto = (presupuesto) => {
+    if (Number(presupuesto) > 0) {
+      console.log('Presupuesto valido')
+    } else {
+      Alert.alert('Error', 'El presupuesto no puede ser 0 o menor', 
+        'Ok'
+      )
+      
+    }
+  }
+
   return (
     <View style={styles.contenedor}>
       <View style={styles.header}>
         <Header />
-        <NuevoPresupuesto />
+        <NuevoPresupuesto 
+          handleNuevoPresupuesto={handleNuevoPresupuesto}
+        />
       </View>
     </View>
   );
--- react-native/planificador/src/components/NuevoPresupuesto.js
+++ react-native/planificador/src/components/NuevoPresupuesto.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 import { View, Text,TextInput, Pressable, StyleSheet } from 'react-native'
 
-const NuevoPresupuesto = () => {
+
+const NuevoPresupuesto = ({handleNuevoPresupuesto}) => {
+  const [presupuesto, setPresupuesto] = useState(0)
+
   return (
     <View style={styles.contenedor}>
-      <Text>Definir Presupuesto</Text>
+      <Text style={styles.label}>Definir Presupuesto</Text>
 
-      <TextInput />
+      <TextInput 
+        keyboardType='numeric'
+        placeholder='Agrega tu presupuesto: Ej. 300'
+        style={styles.input}
+        value={ presupuesto.toString() }
+        onChangeText={setPresupuesto}
+      />
 
-      <Pressable>
-        <Text>Agregar Presupuesto</Text>
+      <Pressable style={styles.boton}
+        onPress={() => handleNuevoPresupuesto(presupuesto)}
+      >
+        <Text style={styles.botonTexto}>Agregar Presupuesto</Text>
       </Pressable>
     </View>
   )
@@ -32,6 +44,34 @@ const styles = StyleSheet.create({
     shadowRadius: 2.62,
 
     elevation: 4,
+  },
+
+  label: {
+    textAlign:'center',
+    fontSize: 24,
+    color: '#3b82f5',
+  }, 
+
+  input: {
+    backgroundColor: '#f5f5f5',
+    padding: 10,
+    borderRadius: 10,
+    textAlign: 'center',
+    marginTop: 30,
+  },
+
+  boton: {
+    marginTop: 30,
+    backgroundColor: '#1048a4',
+    padding: 10,
+    borderRadius: 10
+  },
+
+  botonTexto: {
+    color: '#fff',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    fontWeight: 'bold'
   }
 })
 
