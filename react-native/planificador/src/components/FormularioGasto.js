8901b0b539e53ccffc4a0857b701fbfdb4564892
i-am-sergio <sergioasensiolamata@hotmail.com>
2024-11-22 18:50:48 +0100

    picker added in the form

--- react-native/planificador/src/components/FormularioGasto.js
+++ react-native/planificador/src/components/FormularioGasto.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Text, SafeAreaView, View, TextInput, StyleSheet, Pressable } from 'react-native'
+import { Picker } from '@react-native-picker/picker'
 
 const FormularioGasto = () => {
   return (
@@ -21,12 +22,30 @@ const FormularioGasto = () => {
         </View>
 
         <View>
-          <Text>cantidad Gasto</Text>
+          <Text>Cantidad Gasto</Text>
           <TextInput 
             placeholder='Cantidad del gasto. ej. 300'
             keyboardType='numeric'
           />
         </View>
+
+        <View>
+          <Text>Categoría Gasto</Text>
+          <Picker>
+            <Picker.Item label='-- Seleccione --' value=''/>
+            <Picker.Item label='Ahorro' value='ahorro'/>
+            <Picker.Item label='Comida' value='comida'/>
+            <Picker.Item label='Casa' value='casa'/>
+            <Picker.Item label='Ocio' value='ocio'/>
+            <Picker.Item label='Salud' value='salud'/>
+            <Picker.Item label='Suscripciones' value='suscripciones'/>
+            <Picker.Item label='Gastos Varios' value='gastosvarios'/>
+          </Picker>
+        </View>
+
+        <Pressable>
+          <Text>Agregar Gasto</Text>
+        </Pressable>
 
       </View>
     </SafeAreaView>
